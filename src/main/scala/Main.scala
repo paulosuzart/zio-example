7f0d9194fb5bec5ef03e0ed89e5dc8aae13cc0b2
paulosuzart <paulosuzart@gmail.com>
2021-11-18 22:17:09 +0100

    just playing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,6 +11,8 @@ import cats._
 import cats.data._
 import cats.implicits._
 import cats.syntax.semigroup._
+import cats.syntax.traverse._
+import cats.syntax.applicative._
 import scala.language.higherKinds
 import scala.annotation.tailrec
 
@@ -22,33 +24,35 @@ val runtime = zio.Runtime.default
 object Solver:
 
   // recursively searches for the letter that is already present in a set
-  @tailrec private def check(word: List[Char], container: Set[Char] = Set.empty)
-                            (implicit m : Traverse[Set])
-  : Option[Char] =  word match {
-    case x :: _ if m.exists(container) {_ === x} => Some(x)
-    case x :: xs => check(xs, container.combine(Set(x)))
-    case Nil => None
+  @tailrec private def check[F[_], A: Eq](word: List[A], container: F[A])
+                                         (implicit n: Applicative[F],
+                                          c: Semigroup[F[A]],
+                                          m: Traverse[F])
+  : Option[A] = word match {
+    case x :: _ if m.exists(container) {_ === x } => x.some
+    case x :: xs => check(xs, container.combine(n.pure(x)))
+    case Nil => none
   }
 
 
   // just isolates the logic so return is trnslated into a presentable string
-  private def doCheck(word: List[Char]) : zio.UIO[String] =
-    ZIO.effectTotal(check(word.toList) match {
+  private def doCheck(word: List[Char]): zio.UIO[String] =
+    ZIO.effectTotal(check(word.toList, Set.empty[Char]) match {
       case Some(a) => s"Found repeated '${a}'!"
       case None => "No repetition found :("
     })
 
   // the actual program witht the effects
-  def solve(word : String) =
+  def solve(word: String) =
     for {
       res <- doCheck(word.toList)
     } yield res
 
 
-object HelloWorld extends App:
+object HelloWorld extends App :
   // Create HTTP route
   val app = HttpApp.collectM {
-    case req @ Method.GET -> Root / "find_repeated" => req.url.queryParams.get("word") match {
+    case req@Method.GET -> Root / "find_repeated" => req.url.queryParams.get("word") match {
       case Some(w :: _) => Solver.solve(w).map(Response.text(_))
       case _ => UIO(Response.fromHttpError(HttpError.BadRequest("No word provided")))
     }
