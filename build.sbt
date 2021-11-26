90b7cd77ffb2cd43bccdd45b878bc8bdf6a136a0
paulosuzart <paulosuzart@gmail.com>
2021-11-26 12:53:48 +0100

    Adding tests

--- build.sbt
+++ build.sbt
@@ -1,17 +1,20 @@
+val zioVersion = "1.0.12"
 lazy val root = project
   .in(file("."))
   .settings(
-    name := "scala3-example-project",
-    description := "Example sbt project that compiles using Scala 3",
-    version := "0.1.0",
+    name         := "scala3-example-project",
+    description  := "Example sbt project that compiles using Scala 3",
+    version      := "0.1.0",
     scalaVersion := "3.1.0",
-    libraryDependencies ++=  Seq(
-        "dev.zio" %% "zio" % "1.0.12",
-        "dev.zio" %% "zio-kafka" % "0.17.1",
-        "dev.zio" %% "zio-streams" % "1.0.12",
-//        "dev.io" %% "zio-json" % "0.2.0-M2",
-        "io.d11" %% "zhttp" % "1.0.0.0-RC17",
-        "org.typelevel" %% "cats-core" % "2.6.1",
-        "org.typelevel" %% "alleycats-core" % "2.6.1",
-    )
+    libraryDependencies ++= Seq(
+      "dev.zio" %% "zio"          % zioVersion,
+      "dev.zio" %% "zio-kafka"    % "0.17.1",
+      "dev.zio" %% "zio-streams"  % zioVersion,
+      "dev.zio" %% "zio-test"     % zioVersion % "test",
+      "dev.zio" %% "zio-test-sbt" % zioVersion % "test",
+      "io.d11"        %% "zhttp"          % "1.0.0.0-RC17",
+      "org.typelevel" %% "cats-core"      % "2.6.1",
+      "org.typelevel" %% "alleycats-core" % "2.6.1"
+    ),
+    testFrameworks += new TestFramework("zio.test.sbt.ZTestFramework")
   )
---
+++ src/test/scala/io/bpp/TaskQueueSpec.scala
@@ -0,0 +1,61 @@
+package io.bpp
+
+import io.bpp.taskqueue.Entry
+import io.bpp.taskqueue.TaskQueue
+import zio.test.DefaultRunnableSpec
+import zio.ZRef
+import zio.test._
+import zio.kafka.consumer.CommittableRecord
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import zio.kafka.consumer.Offset
+import zio.ZIO
+import zio.console.Console
+
+import zio.test.Assertion.{isEmpty, equalTo}
+import zio.test.TestAspect.sequential
+import org.scalatest._
+
+import io.bpp.taskqueue.GlobalSet
+
+object TaskQueueSpec extends DefaultRunnableSpec {
+  import TaskQueue._
+
+  val store   = TaskQueue.storeLayer
+  val testEnv = (Console.live ++ store) >>> TaskQueue.live
+
+  def spec = suite("Handle should add entry to the global map")(
+    testM("Add entry to ref correctly") {
+      val record            = new ConsumerRecord("topic", 0, 21, "a-key", "a-value")
+      val committableRecord = CommittableRecord[String, String](record, Map.empty, None)
+
+      for {
+        _ <- handle(committableRecord)
+        x <- ZIO.access[GlobalSet](_.get)
+        c <- x.get
+      } yield assert(c)(equalTo(Map("a-key" -> Entry("a-key", "a-value"))))
+    },
+    testM("Drop non existing entry from ref keeps it unchanged") {
+      val record2 =
+        new ConsumerRecord[String, String]("topic", 0, 21, "a-key2", null.asInstanceOf[String])
+      val tombstone = CommittableRecord[String, String](record2, Map.empty, None)
+
+      for {
+        _ <- handle(tombstone)
+        x <- ZIO.access[GlobalSet](_.get)
+        c <- x.get
+      } yield assert(c)(equalTo(Map("a-key" -> Entry("a-key", "a-value"))))
+    },
+    testM("Drop entry from ref correctly") {
+      val record2 =
+        new ConsumerRecord[String, String]("topic", 0, 21, "a-key", null.asInstanceOf[String])
+      val tombstone = CommittableRecord[String, String](record2, Map.empty, None)
+
+      for {
+        _ <- handle(tombstone)
+        x <- ZIO.access[GlobalSet](_.get)
+        c <- x.get
+      } yield assert(c)(isEmpty)
+    }
+  ).provideCustomLayerShared(store ++ testEnv) @@ sequential
+
+}
